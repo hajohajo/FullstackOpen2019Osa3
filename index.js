41736d1c5cc79cd1a5821ad35bb2cfb252a127d8
hajohajo <joona.havukainen@helsinki.fi>
2019-11-27 16:11:42 +0200

    3.8* puhelinluettelo: Morgan custom token

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
+morgan.token('body', function (req){
+    if(req.method === "POST") {
+        return JSON.stringify(req.body)
+    } else {
+        return " "
+    }
+})
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :res[content-length] - :response-time ms :body'))
 
 let notes = [
     {
