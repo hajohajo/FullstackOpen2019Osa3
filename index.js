403a18151e0730097f61175d2d9ea626637ad227
hajohajo <joona.havukainen@helsinki.fi>
2019-11-29 14:25:25 +0200

    PORT for heroku

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ app.post('/api/persons', (req, res) => {
     res.json(notes)
 })
 
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
