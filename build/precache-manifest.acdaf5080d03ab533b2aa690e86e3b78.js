939362c6fddc1d0cf6a38cf997eb1a9a1d02ff59
hajohajo <joona.havukainen@helsinki.fi>
2019-11-29 15:43:46 +0200

    3.11 debugging

---
+++ build/precache-manifest.acdaf5080d03ab533b2aa690e86e3b78.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5ecef4dd7895200da92bdd73f07194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f41adac2fe42e635c609",
+    "url": "/static/js/2.d887571b.chunk.js"
+  },
+  {
+    "revision": "4d9aa64f8236bb293b09",
+    "url": "/static/js/main.128819e0.chunk.js"
+  },
+  {
+    "revision": "269ad4d029cce48fc3f0",
+    "url": "/static/js/runtime-main.b44955d6.js"
+  }
+]);
