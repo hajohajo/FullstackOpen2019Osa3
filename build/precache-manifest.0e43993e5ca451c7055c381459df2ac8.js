08393525ba9cc15149ea978ffde412f04a27472f
hajohajo <joona.havukainen@helsinki.fi>
2019-12-01 18:00:16 +0200

    3.15, 3.16 ja 3.17*: poistaminen ja päivittäminen backendiin, virheidenkäsittely middlewareen

---
+++ build/precache-manifest.0e43993e5ca451c7055c381459df2ac8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e78f9c8ea90d63c156844f9cd4e3176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f41adac2fe42e635c609",
+    "url": "/static/js/2.d887571b.chunk.js"
+  },
+  {
+    "revision": "f5559d739d2d9e87b6b0",
+    "url": "/static/js/main.d68a8402.chunk.js"
+  },
+  {
+    "revision": "269ad4d029cce48fc3f0",
+    "url": "/static/js/runtime-main.b44955d6.js"
+  }
+]);
