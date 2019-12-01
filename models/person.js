b082f0f0c327d5bee67827724a6f8b4074ba977b
hajohajo <joona.havukainen@helsinki.fi>
2019-12-01 20:47:28 +0200

    3.20* Minimipituus ja frontend virheilmoitus.

--- models/person.js
+++ models/person.js
@@ -14,8 +14,8 @@ mongoose.connect(url, { useNewUrlParser: true })
         })
 
 const personScheme = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    number: {type: String, required: true, unique: true}
+    name: {type: String, required: true, unique: true, minlength: 3},
+    number: {type: String, required: true, unique: true, minlength: 8}
 })
 
 personScheme.set('toJSON', {
