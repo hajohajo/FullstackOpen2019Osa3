78b4af7d3be1f12fa1f7765db72ad1cc9f5c971e
hajohajo <joona.havukainen@helsinki.fi>
2019-12-01 20:54:11 +0200

    3.20* Minimipituus ja frontend virheilmoitus.

---
+++ build/precache-manifest.1fdffacd4f82a228efbd81a382a08d88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d110482f9ff7abacd99f20e26d879258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f41adac2fe42e635c609",
+    "url": "/static/js/2.d887571b.chunk.js"
+  },
+  {
+    "revision": "de1355d43e3b644d580d",
+    "url": "/static/js/main.53ed016c.chunk.js"
+  },
+  {
+    "revision": "269ad4d029cce48fc3f0",
+    "url": "/static/js/runtime-main.b44955d6.js"
+  }
+]);
